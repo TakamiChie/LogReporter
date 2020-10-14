79de8b61c0923a3e6d90f5a6356644df50fe89b2
TakamiChie@Visnu <chie@onpu-tamago.net>
2020-10-14 14:15:00 +0900

    region Comment settings. Sort test methods.

--- tests/test_reporter_log_handler.py
+++ tests/test_reporter_log_handler.py
@@ -10,12 +10,35 @@ class TestReporterLogHandler(unittest.TestCase):
   A test class that verifies the operation of `ReporterLogHandler`.
   """
 
+  #region constructor test
+
+  def test_original_path(self):
+    """
+    If you specify your own file path as an argument of the constructor, check that the file is created in the specified folder.
+    """
+    path = Path(__file__).parent / "out" / "test.log"
+    rlh = ReporterLogHandler(path)
+    logger = logging.getLogger("testlogger")
+    logger.addHandler(rlh)
+    logger.warn("test message")
+    self.assertTrue(path.exists())
+    rlh.get_text()
+    self.assertFalse(path.exists())
+
+  #endregion
+
+  #region prepare
+
   def tearDown(self):
     """
     Executed for each test method call.
     """
     logger = logging.getLogger("testlogger")
     logger.handlers.clear()
+
+  #endregion
+
+  #region get_text() test
 
   def test_get_text_warning(self):
     """
@@ -71,6 +94,10 @@ class TestReporterLogHandler(unittest.TestCase):
     logger.warn("test message")
     self.assertFalse(rlh.get_text() == "")
 
+  #endregion
+
+  #region clear() test
+
   def test_clear(self):
     """
     Confirm that the existing log is deleted when `ReportLogHandler#clear()` is executed without calling `ReportLogHandler#get_text()` after outputting some log.
@@ -83,15 +110,4 @@ class TestReporterLogHandler(unittest.TestCase):
     rlh.clear()
     self.assertFalse(rlh._filename.exists())
 
-  def test_original_path(self):
-    """
-    If you specify your own file path as an argument of the constructor, check that the file is created in the specified folder.
-    """
-    path = Path(__file__).parent / "out" / "test.log"
-    rlh = ReporterLogHandler(path)
-    logger = logging.getLogger("testlogger")
-    logger.addHandler(rlh)
-    logger.warn("test message")
-    self.assertTrue(path.exists())
-    rlh.get_text()
-    self.assertFalse(path.exists())
+  #endregion
